f2b0e708f45a28c35d409e9bc68e4b4d0a66d187
Thais <thais_fernandes62@hotmail.com>
2021-10-20 22:04:36 -0300

    primeiros controllers

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,6 +1,29 @@
-const getUsers = (req, res) => {
-    console.log("você também pode utilizar o console para visualizar =)")
-    res.send("Request getUsers feita")
-  }
-  
-  module.exports = { getUsers }
+const { users } = require('../db/models')
+
+// só ajustar a resposta, por enquanto testando o que são parâmetros da requisição; tratamento de erro
+const getUsers = async (req, res) => {
+  const Users = await users.findAll({
+    attributes: { exclude: ['password'] }
+  });
+  res.status(200).json(req.params.uid);
+}
+
+// Ajustar o uid, tratamento de erro
+const getUserbyId = async (req, res) => {
+  const userbyId = await users.findAll({
+    where: {
+      id: req.body.id,
+    },
+    attributes: { exclude: ['password'] }
+  });
+  res.status(200).json(userbyId);
+}
+
+// tratamento de erro
+const postUsers = async (req, res) => {
+  const { name, email, password, role, restaurant } = req.body
+  await users.create({ name, email, password, role, restaurant })
+  res.status(200).send("usuário adicionado com sucesso")
+}
+
+module.exports = { getUsers, postUsers, getUserbyId }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
 const { Router } = require('express')
-const ExampleRouter = require("./ExampleRouter")
 const usersRouter = require('../routes/usersRouter')
 const productsRouter = require('../routes/productsRouter')
 const ordersRouter = require('../routes/ordersRouter')
@@ -7,7 +6,6 @@ const ordersRouter = require('../routes/ordersRouter')
 const router = Router()
 
 // aqui vai todas as rotas
-router.use('/example', ExampleRouter);
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
 router.use('/orders', ordersRouter);
