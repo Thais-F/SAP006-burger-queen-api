11054937daebba497e26bb4eef14287460446e8f
Thais <thais_fernandes62@hotmail.com>
2021-10-27 21:24:31 -0300

    postgres local como banco de desenvolvimento

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') { require('dotenv').config(); }
 module.exports = {
   "development": {
     "username": "postgres",
-    "password": "Postgres@970729",
+    "password": "admin97",
     "database": "Magic-Burger",
     "host": "127.0.0.1",
     "dialect": "postgres",
